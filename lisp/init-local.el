5d62cc0f1a452dda26f1c812509f724f1541449f
Michael Winton <wintonmr@gmail.com>
2018-04-02 16:17:48 +1200

    Load custom setup

---
+++ lisp/init-local.el
@@ -0,0 +1,9 @@
+(require-package 'reveal-in-osx-finder)
+
+(require 'init-mw-fonts)
+(require 'init-mw-movement)
+(require 'init-mw-yasnippet)
+(require 'init-mw-rspec-mode)
+(require 'init-mw-ispell)
+
+(provide 'init-local)
