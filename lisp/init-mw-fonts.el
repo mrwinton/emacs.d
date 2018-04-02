fef4767076d13ab0d49ac2ca92948a95f49aa0fa
Michael Winton <wintonmr@gmail.com>
2018-04-02 16:16:43 +1200

    Add custom fonts feature

---
+++ lisp/init-mw-fonts.el
@@ -0,0 +1,16 @@
+(defun set-font-bau ()
+  (interactive)
+  (set-font-size 120))
+
+(defun set-font-pairing-station ()
+  (interactive)
+  (set-font-size 160))
+
+(defun set-font-presentation ()
+  (interactive)
+  (set-font-size 220))
+
+(defun set-font-size (font-height)
+  (custom-set-faces `(default ((t (:height ,font-height :family "menlo"))))))
+
+(provide 'init-mw-fonts)
